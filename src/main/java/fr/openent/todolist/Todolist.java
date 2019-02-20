bcea18e4af09dd5f24ce6b6e07f93ff8c1fe2e9c
LEDUNOIS Simon <simon.ledunois@cgi.com>
2019-02-20 15:10:42 +0100

    Init project

---
+++ src/main/java/fr/openent/todolist/Todolist.java
@@ -0,0 +1,12 @@
+package fr.openent.todolist;
+
+import org.entcore.common.http.BaseServer;
+
+public class Todolist extends BaseServer {
+
+	@Override
+	public void start() throws Exception {
+		super.start();
+	}
+
+}
